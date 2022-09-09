724d74c1db675bcbbfebf5dc3c1704197cb16772
Finn Lidbetter <finnlidbetter@gmail.com>
2022-09-08 21:29:42 -0400

    Change root url to rerack subdomain

--- islobsterble/AppDelegate.swift
+++ islobsterble/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import SwiftUI
 
 //let ROOT_URL = "http://192.168.0.16:5001/"
-let ROOT_URL = "https://slobsterble.finnlidbetter.com/"
+let ROOT_URL = "https://rerack.finnlidbetter.com/"
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
